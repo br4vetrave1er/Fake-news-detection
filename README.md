4544f5a393e7dbd9bd81c529a9b6baf3ae24e458
Akshay Kumar <46802910+inDefinate9@users.noreply.github.com>
2021-04-19 20:10:41 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,37 @@
+## Fake news Detectation
+  Machine learning based project for detecting if the given news is fake or not and deployment of the project using Flask.
+
+## Pre-Requisites
+  - sklearn
+  - matplotlib
+  - nltk
+  - numpy
+  - flask
+  - pipeline
+
+## Dataset
+
+  Dataset used is about 2016 U.S. elections during which there was lot of counterfiet information was being shared on social meadia platforms. Our dataset contains 3 feilds.
+  - Title : News Headline
+  - Author : name of the news provider
+  - Text : complete story
+
+  Download dataset through the following link.
+  https://mega.nz/folder/99gVhYBR#Xe74dH31S2qkUsyxCps8Iw
+## Usage
+
+  After installing all the necessary libraries
+  - Run main.py file(Running this script may take some time)
+  - Now deploy the project by running app.py script.
+  - Go to https://127.0.0.1:8080/
+
+## Input
+
+  Provide the data from the test.csv file.
+
+![Image]()  
+
+## Algorithms Used
+
+  - Logistic Regression
+  - Multinomial NB
---
+++ temp.py
@@ -0,0 +1,185 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.pipeline import Pipeline
+from sklearn.externals import joblib
+from sklearn import linear_model
+from sklearn.feature_extraction.text import TfidfTransformer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+import numpy as np # linear algebra
+import pandas as pd #data processing
+import os
+import re
+import nltk
+
+train=pd.read_csv('./fake-news/train.csv')
+test=pd.read_csv('./fake-news/test.csv')
+
+test=test.fillna(' ')
+train=train.fillna(' ')
+test['total']=test['title']+' '+test['author']+test['text']
+train['total']=train['title']+' '+train['author']+train['text']
+
+real_words = ''
+fake_words = ''
+stop_words = set(stopwords.words('english'))
+
+# iterate through the csv file
+for val in train[train['label']==1].total:
+
+    # split the value
+    tokens = val.split()
+
+    # Converts each token into lowercase
+    for i in range(len(tokens)):
+        tokens[i] = tokens[i].lower()
+
+    real_words += " ".join(tokens)+" "
+
+for val in train[train['label']==0].total:
+
+    # split the value
+    tokens = val.split()
+
+    # Converts each token into lowercase
+    for i in range(len(tokens)):
+        tokens[i] = tokens[i].lower()
+
+    fake_words += " ".join(tokens)+" "
+
+
+wordcloud = WordCloud(width = 800, height = 800,
+                background_color ='white',
+                stopwords = stop_words,
+                min_font_size = 10).generate(real_words)
+
+# plot the WordCloud image
+plt.figure(figsize = (8, 8), facecolor = None)
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.tight_layout(pad = 0)
+
+plt.show()
+
+
+
+
+
+
+lemmatizer=WordNetLemmatizer()
+
+lemmatizer=WordNetLemmatizer()
+for index,row in train.iterrows():
+    filter_sentence = ''
+
+    sentence = row['total']
+    sentence = re.sub(r'[^\w\s]','',sentence) #cleaning
+
+    words = nltk.word_tokenize(sentence) #tokenization
+
+    words = [w for w in words if not w in stop_words]  #stopwords removal
+
+    for word in words:
+        filter_sentence = filter_sentence + ' ' + str(lemmatizer.lemmatize(word)).lower()
+
+    train.loc[index,'total'] = filter_sentence
+
+train = train[['total','label']]
+
+
+
+X_train = train['total']
+Y_train = train['label']
+
+corpus = [
+    'This is the first document.',
+    'This document is the second document.',
+    'And this is the third one.',
+    'Is this the first document?',
+]
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(corpus)
+print(vectorizer.get_feature_names())
+
+def vectorize_text(features, max_features):
+    vectorizer = TfidfVectorizer( stop_words='english',
+                            decode_error='strict',
+                            analyzer='word',
+                            ngram_range=(1, 2),
+                            max_features=max_features
+                            #max_df=0.5 # Verwendet im ML-Kurs unter Preprocessing
+                            )
+    feature_vec = vectorizer.fit_transform(features)
+    return feature_vec.toarray()
+
+#Feature extraction using count vectorization and tfidf.
+count_vectorizer = CountVectorizer()
+count_vectorizer.fit_transform(X_train)
+freq_term_matrix = count_vectorizer.transform(X_train)
+tfidf = TfidfTransformer(norm="l2")
+tfidf.fit(freq_term_matrix)
+tf_idf_matrix = tfidf.fit_transform(freq_term_matrix)
+
+tf_idf_matrix
+
+test_counts = count_vectorizer.transform(test['total'].values)
+test_tfidf = tfidf.transform(test_counts)
+
+#split in samples
+
+X_train, X_test, y_train, y_test = train_test_split(tf_idf_matrix, Y_train, random_state=0)
+
+logreg = LogisticRegression(C=1e5)
+logreg.fit(X_train, y_train)
+pred = logreg.predict(X_test)
+print('Accuracy of Lasso classifier on training set: {:.2f}'
+     .format(logreg.score(X_train, y_train)))
+print('Accuracy of Lasso classifier on test set: {:.2f}'
+     .format(logreg.score(X_test, y_test)))
+
+cm = confusion_matrix(y_test, pred)
+cm
+
+
+NB = MultinomialNB()
+NB.fit(X_train, y_train)
+pred = NB.predict(X_test)
+print('Accuracy of NB  classifier on training set: {:.2f}'
+     .format(NB.score(X_train, y_train)))
+print('Accuracy of NB classifier on test set: {:.2f}'
+     .format(NB.score(X_test, y_test)))
+cm = confusion_matrix(y_test, pred)
+cm
+
+#Assiging the variables again as once transformed vectors can't be transformed again using pipeline.
+X_train = train['total']
+Y_train = train['label']
+
+
+
+
+pipeline = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer(norm='l2')),
+    ('clf', linear_model.LogisticRegression(C=1e5)),
+])
+
+pipeline.fit(X_train, Y_train)
+
+pipeline.predict(["flynn hillary clinton big woman campus breitbart daniel j flynnever get feeling life circle roundabout rather head straight line toward intended destination hillary clinton remains big woman campus leafy liberal wellesley massachusetts everywhere else vote likely inauguration dress remainder day way miss havisham forever wore wedding dress speaking great expectations hillary rodham overflowed 48 year ago first addressed wellesley graduating class the president college informed gathered 1969 student needed debate far i could ascertain spokesman kind like democratic primary 2016 minus term unknown even seven sisters school i glad miss adams made clear i speaking today u 400 u miss rodham told classmate after appointing edger bergen charlie mccarthys mortimer snerds attendance bespectacled granny glass awarding matronly wisdom least john lennon wisdom took issue previous speaker despite becoming first win election seat u s senate since reconstruction edward brooke came criticism calling empathy goal protestors criticized tactic though clinton senior thesis saul alinsky lamented black power demagogue elitist arrogance repressive intolerance within new left similar word coming republican necessitated brief rebuttal trust rodham ironically observed 1969 one word i asked class rehearsal wanted say everyone came said talk trust talk lack trust u way feel others talk trust bust what say what say feeling permeates generation perhaps even understood distrusted the trust bust certainly busted clintons 2016 plan she certainly even understand people distrusted after whitewater travelgate vast conspiracy benghazi missing email clinton found distrusted voice friday there load compromising road broadening political horizon and distrust american people trump edged 48 percent 38 percent question immediately prior novembers election stood major reason closing horizon clinton described vanquisher supporter embracing lie con alternative fact assault truth reason she failed explain american people chose lie truth as history major among today know well people power invent fact attack question mark beginning end free society offered that hyperbole like many people emerge 1960s hillary clinton embarked upon long strange trip from high school goldwater girl wellesley college republican president democratic politician clinton drank time place gave degree more significantly went idealist cynic comparison two wellesley commencement address show way back lamented long leader viewed politics art possible challenge practice politics art making appears impossible possible now big woman campus odd woman white house wonder current station even possible why arent i 50 point ahead asked september in may asks isnt president the woman famously dubbed congenital liar bill safire concludes lie mind getting stood election day like finding jilted bride wedding day inspires dangerous delusion"])
+
+#saving the pipeline
+filename = 'pipeline.sav'
+joblib.dump(pipeline, filename)
+
+filename = './pipeline.sav'
+
+loaded_model = joblib.load(filename)
+result = loaded_model.predict(["flynn hillary clinton big woman campus breitbart daniel j flynnever get feeling life circle roundabout rather head straight line toward intended destination hillary clinton remains big woman campus leafy liberal wellesley massachusetts everywhere else vote likely inauguration dress remainder day way miss havisham forever wore wedding dress speaking great expectations hillary rodham overflowed 48 year ago first addressed wellesley graduating class the president college informed gathered 1969 student needed debate far i could ascertain spokesman kind like democratic primary 2016 minus term unknown even seven sisters school i glad miss adams made clear i speaking today u 400 u miss rodham told classmate after appointing edger bergen charlie mccarthys mortimer snerds attendance bespectacled granny glass awarding matronly wisdom least john lennon wisdom took issue previous speaker despite becoming first win election seat u s senate since reconstruction edward brooke came criticism calling empathy goal protestors criticized tactic though clinton senior thesis saul alinsky lamented black power demagogue elitist arrogance repressive intolerance within new left similar word coming republican necessitated brief rebuttal trust rodham ironically observed 1969 one word i asked class rehearsal wanted say everyone came said talk trust talk lack trust u way feel others talk trust bust what say what say feeling permeates generation perhaps even understood distrusted the trust bust certainly busted clintons 2016 plan she certainly even understand people distrusted after whitewater travelgate vast conspiracy benghazi missing email clinton found distrusted voice friday there load compromising road broadening political horizon and distrust american people trump edged 48 percent 38 percent question immediately prior novembers election stood major reason closing horizon clinton described vanquisher supporter embracing lie con alternative fact assault truth reason she failed explain american people chose lie truth as history major among today know well people power invent fact attack question mark beginning end free society offered that hyperbole like many people emerge 1960s hillary clinton embarked upon long strange trip from high school goldwater girl wellesley college republican president democratic politician clinton drank time place gave degree more significantly went idealist cynic comparison two wellesley commencement address show way back lamented long leader viewed politics art possible challenge practice politics art making appears impossible possible now big woman campus odd woman white house wonder current station even possible why arent i 50 point ahead asked september in may asks isnt president the woman famously dubbed congenital liar bill safire concludes lie mind getting stood election day like finding jilted bride wedding day inspires dangerous delusion"])
+print(result)
